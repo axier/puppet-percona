cf0d76cba3845dcc94940f2f9f012971a7d1ec14
Axier Artola <axier1986@gmail.com>
2016-02-29 16:14:58 +0100

    root_password fixed

--- manifests/root_password.pp
+++ manifests/root_password.pp
@@ -31,18 +31,32 @@ class percona::root_password {
       }
     }
     'server': {
-      percona_user { "${user}@${host}":
-        ensure        => present,
-        password_hash => mysql_password($percona::root_password),
-        mgmt_cnf      => $percona::mgmt_cnf,
-        before        => File[$percona::mgmt_cnf],
+      if $percona::root_password != 'UNSET' {
+        if !percona_check_file($percona::mgmt_cnf) {
+          exec {'percona-root-password':
+            onlyif    => [
+              'test -f /usr/bin/mysqladmin',
+              "mysqladmin --no-defaults -u${user} -h${host} status",
+            ],
+            unless    => "test -f $percona::mgmt_cnf",
+            path      => ['/usr/bin','/bin',],
+            command   => "mysqladmin -h ${host} -u${user} password ${password}",
+            before    => File[$percona::mgmt_cnf],
+          }
+        } else {
+          percona_user { "${user}@${host}":
+            ensure        => present,
+            password_hash => mysql_password($percona::root_password),
+            mgmt_cnf      => $percona::mgmt_cnf,
+            before        => File[$percona::mgmt_cnf],
+          }
+        }
       }
     }
     default: {
       # Client doesn't require.
     }
   }
-
   file { $percona::mgmt_cnf:
     owner   => $percona::config_user,
     group   => $percona::config_group,
